4c367df803a0d7e57fdcd808daf5442b67a8859c
wakim <wakim.jraige@gmail.com>
2016-03-12 15:55:31 -0300

    Removed unused class

--- app/src/main/java/br/com/wakim/eslpodclient/podcastplayer/view/QueueManager.kt
+++
@@ -1,8 +0,0 @@
-package br.com.wakim.eslpodclient.podcastplayer.view
-
-import br.com.wakim.eslpodclient.model.PodcastItem
-
-interface QueueManager {
-    fun getNext(podcastItem: PodcastItem) : PodcastItem?
-    fun getPrevious(podcastItem: PodcastItem) : PodcastItem?
-}
